898fac23c882a5c6054963e9dcbe7640e0c7381d
pkhatri196 <payal.khatri02@gmail.com>
2025-01-12 19:03:16 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Details from "./components/Details/Details";
+import Main from "./components/Main/Main";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      className={classes.grid}
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ height: "100vh"  }}
+    >
+      <Grid item xs={12} sm={4}>
+        <Details title="Income" />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Main />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Details title="Expense" />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
